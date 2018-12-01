945073b5a65abdcfc5cc0ee1cb7863f85cb628fe
Alex Duffell <alex.duffell@protirus.com>
2018-12-01 20:23:33 +0000

    Topics are now included in get repos api call. Cards now display repo topics.

--- js/app.js
+++ js/app.js
@@ -5,12 +5,17 @@ myApp.controller('myController', function ($scope, $http) {
         loadRepos();
     };
 
-    loadRepos = () => {
-        $http.get('https://api.github.com/orgs/protirus/repos')
-        .then(function(response) {
-            //$scope.repos = response.data; //.map(r => r.name);
-            //console.log($scope.repos);
+    var req = {
+        method: 'GET',
+        url: 'https://api.github.com/orgs/protirus/repos',
+        headers: {
+          'Accept': 'application/vnd.github.mercy-preview+json'
+        }
+}
 
+    loadRepos = () => {
+        $http(req)
+        .then(function(response) {
             $scope.repos = sortRepos(response.data);
         });
     }

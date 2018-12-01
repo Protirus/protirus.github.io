d6c36ae792f532bbe18d8e134f4c08d60918c7c3
Alex Duffell <alex.duffell@protirus.com>
2018-12-01 18:29:59 +0000

    Removed github.io site from results & sorted repo's based on last updated

--- js/app.js
+++ js/app.js
@@ -1,20 +1,47 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('myController', function ($scope, $http) {   
 
-    loadRepos = () => {
-        $http.get('https://api.github.com/orgs/protirus/repos')
-        .then(function(response) {
-            $scope.repos = response.data; //.map(r => r.name);
-            console.log($scope.repos);
-        });
-    }
-    
     $scope.init = () => {
         loadRepos();
     };
+
+    loadRepos = () => {
+        $http.get('https://api.github.com/orgs/protirus/repos')
+        .then(function(response) {
+            //$scope.repos = response.data; //.map(r => r.name);
+            //console.log($scope.repos);
+
+            $scope.repos = sortRepos(response.data);
+        });
+    }
+
+    sortRepos = (repos) => {
+        
+        var sortedRepos = [];
+        repos.forEach(element => {
+
+            if (element.name != "protirus.github.io") {
+                sortedRepos.push(element);
+            }
+        });
+
+        sortedRepos.sort(function(a, b) {
+            return new Date(b.date) - new Date(a.date);
+        }).reverse();
+
+        return sortedRepos;
+    }
 
     $scope.OpenRepository = (index) => {
         var repo = $scope.repos[index];
         window.open(repo.html_url);
     }
 });
+
+function compare(first, second) {
+    if (first.last_nom < second.last_nom)
+        return -1;
+    if (first.last_nom > second.last_nom)
+      return 1;
+   return 0;
+}
